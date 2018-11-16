cbd77cf40db8bfac9f7a1f026fb61f4bd9ea36f2
zhulingfeng <664675691@qq.com>
2018-11-16 09:16:21 +0800

    modify setup.py and nms_wrapper.py to support cup-only nms

--- lib/fast_rcnn/nms_wrapper.py
+++ lib/fast_rcnn/nms_wrapper.py
@@ -6,7 +6,6 @@
 # --------------------------------------------------------
 
 from fast_rcnn.config import cfg
-from nms.gpu_nms import gpu_nms
 from nms.cpu_nms import cpu_nms
 from nms.py_cpu_nms import py_cpu_pnms
 
@@ -16,6 +15,7 @@ def nms(dets, thresh, force_cpu=False):
     if dets.shape[0] == 0:
         return []
     if cfg.USE_GPU_NMS and not force_cpu:
+        from nms.gpu_nms import gpu_nms
         return gpu_nms(dets, thresh, device_id=cfg.GPU_ID)
     else:
         return cpu_nms(dets, thresh)
