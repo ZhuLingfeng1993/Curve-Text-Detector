f32bd39bc87e72838ef211d09ae728e2eb2a3eff
zhulingfeng <664675691@qq.com>
2018-11-28 19:32:17 +0800

    add q_rfcn_tloc_with_ref sehll scripts and yml cfgs

---
+++ experiments/scripts/q_rfcn_tloc_with_ref.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+
+GPU_ID=$1
+NET=$2
+NET_lc=${NET,,}
+DATASET=$3
+ITERS=$4
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:4:$len}
+EXTRA_ARGS_SLUG=${EXTRA_ARGS// /_}
+
+TRAIN_IMDB="icdar2015ch4"
+
+LOG="experiments/logs/q_rfcn_tloc_with_ref_with_ref_${NET}_${EXTRA_ARGS_SLUG}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+# icdar2015ch4  replace \n with \\\n or replace \\\n with \n
+time ./tools/train_net.py   \
+  --gpu ${GPU_ID} \
+  --solver models/q_rfcn_tloc_with_ref_with_ref/solver_q_rfcn_tloc_with_ref_with_ref.prototxt \
+  --weights data/imagenet_models/ResNet-50-model.caffemodel \
+  --iters ${ITERS} \
+  --imdb ${TRAIN_IMDB} \
+  --cfg experiments/cfgs/q_rfcn_tloc_with_ref_with_ref.yml \
+  --train_label_list data/icdar2015ch4/ch4_training_localization_transcription_gt.txt \
+  --train_image_list data/icdar2015ch4/ch4_training_images.txt \
+  ${EXTRA_ARGS}
+
+ 
---
+++ test_q_rfcn_tloc_with_ref.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+NET="ResNet-50"
+LOG="experiments/logs/test_q_rfcn_tloc_with_ref_${NET}.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+TEST_IMDB="icdar2015ch4_test"
+
+# icdar2015ch4
+./tools/test_net.py --gpu 3 \
+  --def models/q_rfcn_tloc_with_ref/test_q_rfcn_tloc_with_ref.prototxt \
+  --net output/q_rfcn_tloc_with_ref/icdar2015ch4/q_rfcn_tloc_with_ref_iter_3.caffemodel \
+  --imdb icdar2015ch4_test \
+  --cfg experiments/cfgs/q_rfcn_tloc_with_ref.yml \
+  --test_label data/icdar2015ch4/Challenge4_Test_Task1_GT.txt \
+  --test_image data/icdar2015ch4/ch4_test_images.txt \
+  --vis
+
